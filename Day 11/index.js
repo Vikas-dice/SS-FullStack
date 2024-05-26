ed084e012e3bab7f4a21e2be8a3d93e42493320b
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-05-26 12:10:36 +0530

    code pushed for dom class

---
+++ Day 11/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// setInterval(()=>{
+//     console.log("hello vikas ");
+// setTimeout(()=>{
+//     console.log("hello vikas 3 sec ");
+
+// },3000)
+
+// setTimeout(()=>{
+//     console.log("hello vikas 1 sec ");
+
+// },1000)
+// setTimeout(()=>{
+//     console.log("hello vikas  5 sec");
+
+// },5000)
+
+// },2000)
